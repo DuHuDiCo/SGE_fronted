3bc4747a8ab66e38060561c516fc17b0bf79893f
Duvan Diaz <dudicodiaz@gmail.com>
2022-10-26 17:50:32 -0500

    commit 26/10/2022
    creacion del sidebar(faltan cosas)

--- SGE_frontend/src/app/app.module.ts
+++ SGE_frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './componentesGenerales/sidebar/sidebar.component';
+import { DashboardComponent } from './Pages/Cartera/pages/dashboard/dashboard.component';
+import { NavbarComponent } from './componentesGenerales/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
