7bc004ad0330882f2a1485b2f1c1c2b86da2dbac
Duvan Diaz <dudicodiaz@gmail.com>
2022-10-27 14:27:05 -0500

    Revert "commit 27/10/2022"
    This reverts commit eaa06447941481b777fa10e0a678b756164c6fab.

---
+++ SGE_frontend/src/app/Pages/Cartera/pages/componentesCartera/sidebar-cartera/sidebar-cartera.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-cartera',
+  templateUrl: './sidebar-cartera.component.html',
+  styleUrls: ['./sidebar-cartera.component.css']
+})
+export class SidebarCarteraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- SGE_frontend/src/app/app.module.ts
+++ SGE_frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './componentesGenerales/sidebar/sidebar.component';
 import { DashboardComponent } from './Pages/Cartera/pages/dashboard/dashboard.component';
 import { NavbarComponent } from './componentesGenerales/navbar/navbar.component';
+import { SidebarCarteraComponent } from './Pages/Cartera/pages/componentesCartera/sidebar-cartera/sidebar-cartera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarCarteraComponent
   ],
   imports: [
     BrowserModule,
