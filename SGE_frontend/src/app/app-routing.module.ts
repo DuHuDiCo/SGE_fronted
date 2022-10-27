eaa06447941481b777fa10e0a678b756164c6fab
Duvan Diaz <dudicodiaz@gmail.com>
2022-10-27 14:23:16 -0500

    commit 27/10/2022
    se creo el navbar con los componentes necesarios, el sidebar quedo realizado(sin logica)---esto quedo en la ruta "cartera"

--- SGE_frontend/src/app/app-routing.module.ts
+++ SGE_frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/Cartera/pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"cartera",
+    component:DashboardComponent,
+    children:[]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
