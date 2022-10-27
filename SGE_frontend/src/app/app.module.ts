91abdc681697e77b2ae13d254f2deb18dbb117e9
Duvan Diaz <dudicodiaz@gmail.com>
2022-10-27 14:40:13 -0500

    c

--- SGE_frontend/src/app/app.module.ts
+++ SGE_frontend/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './componentesGenerales/sidebar/sidebar.component';
+<<<<<<< HEAD
 import { DashboardComponent } from './Pages/Cartera/pages/dashboard/dashboard.component';
 import { NavbarComponent } from './componentesGenerales/navbar/navbar.component';
 import { SidebarCarteraComponent } from './Pages/Cartera/pages/componentesCartera/sidebar-cartera/sidebar-cartera.component';
+=======
+>>>>>>> parent of 3bc4747 (commit 26/10/2022)
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     SidebarComponent,
     DashboardComponent,
     NavbarComponent,
     SidebarCarteraComponent
+=======
+    SidebarComponent
+>>>>>>> parent of 3bc4747 (commit 26/10/2022)
   ],
   imports: [
     BrowserModule,
