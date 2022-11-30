ce14fcdfd35323c0e521eec13ffcb7a97290981c
Alejandro correa Uribe <alejandrolol153@gmail.com>
2022-11-30 14:00:49 -0500

    comit

--- SGE_frontend/src/app/app-routing.module.ts
+++ SGE_frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultasComponent } from './ComponentesConsignacion/consultas/consultas.component';
+import { IngresarComponent } from './ComponentesConsignacion/ingresar/ingresar.component';
+import { PestanasComponent } from './ComponentesConsignacion/pestanas/pestanas.component';
+import { ReportesComponent } from './ComponentesConsignacion/reportes/reportes.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo:'pestanas' , pathMatch:'full' },
+  {path:'pestanas', component:PestanasComponent},
+  {
+    path:'consultas', 
+    component:ConsultasComponent,
+    children:[]
+  },
+  {
+    path:'ingresar',
+    component:IngresarComponent,
+    children:[]
+  },
+  {
+    path:'reportes',
+    component:ReportesComponent,
+    children:[]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SGE_frontend/src/app/app.module.ts
+++ SGE_frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PestanasComponent } from './ComponentesConsignacion/pestanas/pestanas.component';
+import { ConsultasComponent } from './ComponentesConsignacion/consultas/consultas.component';
+import { IngresarComponent } from './ComponentesConsignacion/ingresar/ingresar.component';
+import { ReportesComponent } from './ComponentesConsignacion/reportes/reportes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PestanasComponent,
+    ConsultasComponent,
+    IngresarComponent,
+    ReportesComponent
   ],
   imports: [
     BrowserModule,
