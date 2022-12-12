f5f2f48c15087b10538f75d5069933f89f57a57f
Duvan Diaz <dudicodiaz@gmail.com>
2022-12-12 11:42:09 -0500

    commit
    sidebar de cartera organizado para ser utilizado desde cualquier lugar pasandole los datos de cada modulo correspondiente

--- SGE_frontend/src/app/Pages/Cartera/pages/dashboard/dashboard.component.ts
+++ SGE_frontend/src/app/Pages/Cartera/pages/dashboard/dashboard.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  
+  datos = {
+    "tituloPagina": "Departamento de Cartera",
+    "opciones":["Escritorio", "Tasa de Cobro", "Calendario", "Estadisticas", "Facturacion", "Gestion", "Revisiones", "Configuraciones", "Logout"],
+    "clases":["fa-solid fa-house-user", "fa-solid fa-building-columns", "fa-solid fa-calendar-days", "fa-solid fa-chart-simple", "fa-solid fa-file-invoice", "fa-solid fa-credit-card", "fa-solid fa-eye", "fa-solid fa-gear", "fa-solid fa-right-from-bracket"],
+    "urls": ["", "", "", "", "", "/gestion", "", "", ""]
+  }
+   
   constructor() { }
 
   ngOnInit(): void {
