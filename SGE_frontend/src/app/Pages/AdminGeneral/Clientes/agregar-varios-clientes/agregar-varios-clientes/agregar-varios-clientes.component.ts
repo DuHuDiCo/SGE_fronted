a5b66d32f1f35e340d47dceb3d9efc4bbdde416e
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2023-08-16 17:48:25 -0500

    Primeras validaciones de agregar usuario

--- SGE_frontend/src/app/Pages/AdminGeneral/Clientes/agregar-varios-clientes/agregar-varios-clientes/agregar-varios-clientes.component.ts
+++ SGE_frontend/src/app/Pages/AdminGeneral/Clientes/agregar-varios-clientes/agregar-varios-clientes/agregar-varios-clientes.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/Services/authentication/authentication.service';
+import { AgregarVariosClientesService } from 'src/app/Services/clientes/AgregarVarios/agregar-varios-clientes.service';
+import baseUrl from 'src/app/utils/helper';
+import Swal from 'sweetalert2';
+
+declare var window: any;
 
 @Component({
   selector: 'app-agregar-varios-clientes',
@@ -8,14 +15,47 @@ import { Component, OnInit } from '@angular/core';
 
 export class AgregarVariosClientesComponent implements OnInit{
 
+  files:string = ''
+  delimitante:string = ''
+  formdata:FormData = new FormData()
+  formModal:any;
+
+  excepciones:string[] = []
  
- constructor() { }
+ constructor(private authService:AuthenticationService, private http:HttpClient, private variosClientes:AgregarVariosClientesService) { }
 
  ngOnInit(): void {
+
+  this.formModal = new window.bootstrap.Modal(
+    document.getElementById('modal')
+  );
   
   }
 
-  
+  obtenerArchivo(event:any){
+   this.files= event.target.files[0]
+  }
+
+  subirArchivos(){
+    this.formdata.append("files", this.files) 
+    const username = this.authService.getUsername();
+    if(username == null){
+      return
+    }
+    this.variosClientes.subirArchivo(this.formdata, this.delimitante, username).subscribe(
+      (data:any) => {
+        console.log(data);
+        if(data.Exceptions.length > 0){
+          this.excepciones = data.Exceptions
+          Swal.fire('Error de Validacion', `Algunos registros no fueron validados: ${data.Exceptions.length}` , 'error')
+          this.formModal.show();
+          return
+        }
+      },(error:any) => {
+        console.log(error);
+      }
+    )
+  }
 }
 
 
--- SGE_frontend/src/app/Services/clientes/AgregarVarios/agregar-varios-clientes.service.ts
+++ SGE_frontend/src/app/Services/clientes/AgregarVarios/agregar-varios-clientes.service.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import baseUrl from 'src/app/utils/helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgregarVariosClientesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public subirArchivo(file:any, delimitante:string, username:string){
+    return this.http.post(`${baseUrl}/validations/file?delimitante=${delimitante}&username=${username}`, file)
+  }
 
 
 }
