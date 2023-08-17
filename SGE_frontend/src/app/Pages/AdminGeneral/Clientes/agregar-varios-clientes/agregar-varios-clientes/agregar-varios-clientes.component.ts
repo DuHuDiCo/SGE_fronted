9f6614c6be075b968fbaf2433c81cfdf499a7240
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2023-08-17 17:38:44 -0500

    Listar cambios y errores (casi listo)

--- SGE_frontend/src/app/Pages/AdminGeneral/Clientes/agregar-varios-clientes/agregar-varios-clientes/agregar-varios-clientes.component.ts
+++ SGE_frontend/src/app/Pages/AdminGeneral/Clientes/agregar-varios-clientes/agregar-varios-clientes/agregar-varios-clientes.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/Services/authentication/authentication.service';
 import { AgregarVariosClientesService } from 'src/app/Services/clientes/AgregarVarios/agregar-varios-clientes.service';
+import { nuevosClientes } from 'src/app/Types/NuevosClientes';
 import baseUrl from 'src/app/utils/helper';
 import Swal from 'sweetalert2';
 
@@ -21,6 +22,10 @@ export class AgregarVariosClientesComponent implements OnInit{
   formModal:any;
 
   excepciones:string[] = []
+  clientes:nuevosClientes[] = []
+  cambios:string[] = []
+
+  mostrarClientes:boolean = false;
  
  constructor(private authService:AuthenticationService, private http:HttpClient, private variosClientes:AgregarVariosClientesService) { }
 
@@ -47,12 +52,26 @@ export class AgregarVariosClientesComponent implements OnInit{
         console.log(data);
         if(data.Exceptions.length > 0){
           this.excepciones = data.Exceptions
+          this.clientes = data.Clientes
+          this.cambios = data.Cambios       
           Swal.fire('Error de Validacion', `Algunos registros no fueron validados: ${data.Exceptions.length}` , 'error')
           this.formModal.show();
+          this.mostrarClientes = true
           return
         }
       },(error:any) => {
         console.log(error);
+      }
+    )
+  }
+
+  guardarCambios(){
+    this.variosClientes.guardarArchivo(this.clientes).subscribe(
+      (data:any) => {
+        this.clientes = data
+        console.log(data);
+      }, (error:any) => {
+        console.log(error)
       }
     )
   }
--- SGE_frontend/src/app/Services/clientes/AgregarVarios/agregar-varios-clientes.service.ts
+++ SGE_frontend/src/app/Services/clientes/AgregarVarios/agregar-varios-clientes.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { nuevosClientes } from 'src/app/Types/NuevosClientes';
 import baseUrl from 'src/app/utils/helper';
 
 @Injectable({
@@ -11,6 +12,10 @@ export class AgregarVariosClientesService {
 
   public subirArchivo(file:any, delimitante:string, username:string){
     return this.http.post(`${baseUrl}/validations/file?delimitante=${delimitante}&username=${username}`, file)
+  }
+
+  public guardarArchivo(clientes:nuevosClientes[]){
+    return this.http.post(`${baseUrl}/validations/data`, clientes)
   }
 
 
