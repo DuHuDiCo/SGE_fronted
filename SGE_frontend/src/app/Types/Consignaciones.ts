8634f0a184c3987c22545bc8e401d40e83b6c836
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2023-09-07 17:26:55 -0500

    modificaciones en consignaciones

---
+++ SGE_frontend/src/app/Types/Consignaciones.ts
@@ -0,0 +1,9 @@
+export type Consignacion = {
+    numeroRecibo:string,
+    valor:string,
+    fechaPago:Date,
+    idPlataforma:number,
+    observaciones:string,
+    cedula:string,
+    base64:string
+}
