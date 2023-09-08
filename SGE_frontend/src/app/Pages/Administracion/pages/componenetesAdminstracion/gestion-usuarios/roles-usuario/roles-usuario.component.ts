36250757ad7fb28da5b1c54d530037f7f8fc6b19
Duvan Diaz <dudicodiaz@gmail.com>
2023-09-08 17:47:23 -0500

    comit

--- SGE_frontend/src/app/Pages/Administracion/pages/componenetesAdminstracion/gestion-usuarios/roles-usuario/roles-usuario.component.ts
+++ SGE_frontend/src/app/Pages/Administracion/pages/componenetesAdminstracion/gestion-usuarios/roles-usuario/roles-usuario.component.ts
@@ -13,8 +13,13 @@ export class RolesUsuarioComponent implements OnInit {
 
 
 
-  selectedRole: string[] = []
-  selectedPermisos: string[] = []
+  selectedRole: number[] = []
+  selectedPermisos: number[] = []
+
+  usuario: any = {
+    "usuario": {},
+    "roles": []
+  }
 
   permisosUsuarios = {
     idRole: '',
@@ -41,28 +46,121 @@ export class RolesUsuarioComponent implements OnInit {
   }
 
 
-  activarRol(rol: string) {
+  activarRol(rol: number) {
 
     if (this.selectedRole.includes(rol)) {
       var position = this.selectedRole.indexOf(rol)
       this.selectedRole.splice(position, 1)
     } else {
       this.selectedRole.push(rol)
+
     }
+
+
+
   }
 
-  seleccionarAllRoles(rol: string) {
-    const role = this.IterarRol.find((r: any) => r.rol == rol)
+  seleccionarAllRoles(rol: number) {
+    const role = this.IterarRol.find((r: any) => r.idRole == rol)
     if (role != null) {
-      role.permissions.forEach((permi: any) => {
-        this.selectedPermisos.push(permi.permission)
-      });
+      var vali = this.usuario.roles.find((r: any) => r.id == rol)
+
+      if (vali != null || vali != undefined) {
+        if (vali.permisos.length > 0) {
+
+          vali.permisos.forEach((p: any) => {
+            if (this.selectedPermisos.includes(p)) {
+              var position = this.selectedPermisos.indexOf(p)
+              this.selectedPermisos.splice(position, 1)
+
+            }
+          });
+
+          vali.permisos = []
+
+        } else {
+          role.permissions.forEach(element => {
+            this.selectedPermisos.push(element.idPermission)
+          });
+
+          vali.permisos = role.permissions
+
+        }
+
+
+
+      } else {
+
+        var roles: any = []
+
+        role.permissions.forEach((p: any) => {
+          this.selectedPermisos.push(p.idPermission)
+          roles.push(p.idPermission)
+        });
+
+        var roleDto = {
+          "id": rol,
+          "permisos": roles
+        }
+
+        this.usuario.roles.push(roleDto)
+
+      }
+
     }
 
     console.log(this.selectedPermisos);
-    
+
 
   }
+
+  selecionarPermiso(permiso: number, rol: number) {
+    const role = this.IterarRol.find((r: any) => r.idRole == rol)
+    alert(role)
+    if (role != null || role != undefined) {
+      const vali = this.usuario.roles.find((r:any)=>r.id == rol)
+      alert(vali)
+      if(vali != null || vali != undefined){
+        if(vali.permisos > 0){
+
+        }else{
+          this.selectedPermisos.push(permiso)
+
+        }
+      }else{
+        this.selectedPermisos.push(permiso)
+        alert(permiso)
+
+        var rolDto:any = {
+          "id": rol,
+          "permisos":[]
+        }
+
+        role.permissions.forEach(element => {
+          if(this.selectedPermisos.includes(element.idPermission)){
+            rolDto.permisos.push(element.idPermission)
+          }
+        });
+
+        this.usuario.roles.push(rolDto)
+        console.log(this.selectedPermisos);
+        
+      }
+
+    }
+
+   
+
+
+  }
+
+
+  guardar() {
+    console.log(this.usuario);
+
+  }
+
+
 
 
 }
