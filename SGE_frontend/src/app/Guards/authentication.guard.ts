a265338edc73f54ec95b19183d142e8043f70f7d
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2023-10-04 17:37:50 -0500

    cambio en los guards

--- SGE_frontend/src/app/Guards/authentication.guard.ts
+++ SGE_frontend/src/app/Guards/authentication.guard.ts
@@ -2,20 +2,25 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../Services/authentication/authentication.service';
+import { OpcionesService } from '../Services/Opciones/opciones.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  constructor(private authentication: AuthenticationService, private router: Router) { }
+  constructor(private authentication: AuthenticationService, private router: Router, private opcionesService:OpcionesService) { }
 
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.authentication.isLoggedIn()) {
-      return true;
+      if(this.opcionesService.getUpdate()){
+        return true;
+      }
+      this.router.navigate(['cambioContrasena'])
+      return false;
     }
     this.router.navigate(['login'])
     return false;
