eba6ae16750ff7809e55e766a0a3a5a44ec0bd1d
JuasDX <138724620+JuasDX@users.noreply.github.com>
2023-10-26 08:45:43 -0500

    Rankings

--- SGE_frontend/src/app/app.module.ts
+++ SGE_frontend/src/app/app.module.ts
@@ -98,6 +98,7 @@ import { ObligacionesComponent } from './Pages/Consignaciones/pages/componentesC
 import { EstadosObligacionComponent } from './Pages/Consignaciones/pages/componentesConsignaciones/Configuraciones/estados-obligacion/estados-obligacion.component';
 import { TipoObligacionComponent } from './Pages/Consignaciones/pages/componentesConsignaciones/Configuraciones/tipo-obligacion/tipo-obligacion.component';
 import { AsesoresComponent } from './Pages/Consignaciones/pages/componentesConsignaciones/Configuraciones/asesores/asesores.component';
+import { RankingsComponent } from './pages/consignaciones/pages/componentesConsignaciones/rankings/rankings/rankings.component';
 
 
 
@@ -203,6 +204,7 @@ import { AsesoresComponent } from './Pages/Consignaciones/pages/componentesConsi
     EstadosObligacionComponent,
     TipoObligacionComponent,
     AsesoresComponent,
+    RankingsComponent,
 
   ],
 
