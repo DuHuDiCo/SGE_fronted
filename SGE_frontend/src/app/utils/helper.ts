f8f1a41710d105725062325033c790351527be68
Duvan Diaz <dudicodiaz@gmail.com>
2023-11-24 08:19:58 -0500

    c

--- SGE_frontend/src/app/utils/helper.ts
+++ SGE_frontend/src/app/utils/helper.ts
@@ -1,5 +1,5 @@
-// let baseUrl = "https://api-gateway-local-2.duckdns.org/api/v1";
-let baseUrl = "http://192.168.1.241:9000/api/v1";
+let baseUrl = "https://api-gateway-local-2.duckdns.org/api/v1";
+// let baseUrl = "http://192.168.1.241:9000/api/v1";
 export default baseUrl;
 
  
