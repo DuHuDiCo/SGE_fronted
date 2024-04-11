c8772562ed9017073dd8d1d72a4e8c33316d5ec5
Duvan Diaz <dudicodiaz@gmail.com>
2024-04-11 16:24:24 -0500

    c

--- SGE_frontend/src/app/Pages/Cartera/pages/componentesCartera/home-cartera/home-cartera.component.ts
+++ SGE_frontend/src/app/Pages/Cartera/pages/componentesCartera/home-cartera/home-cartera.component.ts
@@ -4610,31 +4610,31 @@ export class HomeCarteraComponent implements OnInit {
     if (user != null || user != undefined) {
       recibo.username = user;
 
-      // this.cuentasCobrar.crearRecibo(recibo).subscribe(
-      //   (data: any) => {
+      this.cuentasCobrar.crearRecibo(recibo).subscribe(
+        (data: any) => {
 
-      //     this.mostrarReciboPago(data.base64)
-      //     this.getGestiones(this.cuentaCobrarSelected.numeroObligacion)
+          this.mostrarReciboPago(data.base64)
+          this.getGestiones(this.cuentaCobrarSelected.numeroObligacion)
 
-      //     this.activarGuardarPago = false
-      //     this.savePago = false
-      //     this.coutasRequest = []
-      //     this.pago = {
-      //       valor: 0,
-      //       detalle: '',
-      //       medioPago: "",
-      //       numeroRecibo: '',
-      //       cumpliendo: false
-      //     }
+          this.activarGuardarPago = false
+          this.savePago = false
+          this.coutasRequest = []
+          this.pago = {
+            valor: 0,
+            detalle: '',
+            medioPago: "",
+            numeroRecibo: '',
+            cumpliendo: false
+          }
 
-      //     this.valorTotalIngresado = 0
-      //   }, (error: any) => {
-      //     this.activarGuardarPago = false
-      //     this.savePago = false
-      //     console.log(error);
+          this.valorTotalIngresado = 0
+        }, (error: any) => {
+          this.activarGuardarPago = false
+          this.savePago = false
+          console.log(error);
 
-      //   }
-      // )
+        }
+      )
     }
 
     $('#modalGestionCom').modal('hide');
