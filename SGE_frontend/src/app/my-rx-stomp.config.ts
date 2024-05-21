3d8bf7c4114249cfcabee36a88bd0573bf2d7978
Duvan Diaz <dudicodiaz@gmail.com>
2024-05-21 16:22:13 -0500

    commit

--- SGE_frontend/src/app/my-rx-stomp.config.ts
+++ SGE_frontend/src/app/my-rx-stomp.config.ts
@@ -6,6 +6,9 @@ import * as SockJS from "sockjs-client";
 
 export const MyRxStompConfig: RxStompConfig = {
     webSocketFactory: () => new SockJS("http://192.168.1.241:8030/our-websocket"),
+    connectHeaders:{
+        "user_id":"admin"
+    },
     heartbeatIncoming: 0,
     heartbeatOutgoing: 20000,
     reconnectDelay: 5000,
