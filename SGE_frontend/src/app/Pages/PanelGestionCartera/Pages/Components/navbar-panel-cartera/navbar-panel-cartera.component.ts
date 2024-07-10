6f5c5bfd8536d24cdc8cc79c1b215b68e1a15f11
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2024-07-10 15:40:28 -0500

    primer panel terminado

--- SGE_frontend/src/app/Pages/PanelGestionCartera/Pages/Components/navbar-panel-cartera/navbar-panel-cartera.component.ts
+++ SGE_frontend/src/app/Pages/PanelGestionCartera/Pages/Components/navbar-panel-cartera/navbar-panel-cartera.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarPanelCarteraComponent implements OnInit {
 
+  modoGestion: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
