0b88c6776400f5d22986a4a5b208c2790afb5854
4rtemis-25 <121249378+4rtemis-25@users.noreply.github.com>
2024-08-29 14:16:34 -0500

    Primeras correciones

--- SGE_frontend/src/app/Pages/Archivos/componentes_archivos/tipo-archivo/tipo-archivo.component.ts
+++ SGE_frontend/src/app/Pages/Archivos/componentes_archivos/tipo-archivo/tipo-archivo.component.ts
@@ -3,6 +3,7 @@ import { TipoArchivoService } from 'src/app/Services/Archivo/TipoArchivo/tipo-ar
 import { TipoArchivo } from 'src/app/Types/Archivo/TipoArchivo';
 import Swal from 'sweetalert2';
 
+declare var $: any;
 @Component({
   selector: 'app-tipo-archivo',
   templateUrl: './tipo-archivo.component.html',
@@ -10,136 +11,108 @@ import Swal from 'sweetalert2';
 })
 export class TipoArchivoComponent implements OnInit {
 
-  tipoArchivo:TipoArchivo = {
+  tipoArchivo: TipoArchivo = {
     tipoArchivo: '',
     idTipoArchivo: 0
   }
 
-  modal:TipoArchivo = {
+  modal: TipoArchivo = {
     idTipoArchivo: 0,
     tipoArchivo: ''
   }
 
-  tiposArchivos:TipoArchivo[] = []
+  tiposArchivos: TipoArchivo[] = []
 
   rolesArray: string[] = ['Cartera', 'Caja', 'Archivos', 'Ventas', 'Servicios', 'Consignaciones', 'SUPERADMINISTRADOR', 'SST']
   permisos: string[] = ['ELIMINAR ARCHIVOS', 'EDITAR ARCHIVOS', 'SUBIR UN ARCHIVO', 'SUBIR ARCHIVOS', 'CREAR TIPOS ARCHIVO', 'EDITAR TIPOS ARCHIVO', 'BUSCAR ARCHIVOS']
 
-  botonGuardarTipo:boolean = false
-  botonEliminar:boolean = false
-  botonEditar:boolean = false
-  cedula:string = ''
+  cedula: string = ''
 
-  constructor(private tipoArchivoService:TipoArchivoService) { }
+  constructor(private tipoArchivoService: TipoArchivoService) { }
 
   ngOnInit(): void {
     this.getAll()
   }
 
-  getAll(){
+  // OBTENER TIPOS DE ARCHIVOS
+  getAll() {
     this.tipoArchivoService.getAll().subscribe(
-      (data:any) => {
+      (data: any) => {
         this.tiposArchivos = data
-      }, (error:any) => {
+        console.log(this.tiposArchivos);
+      }, (error: any) => {
         console.log(error);
       }
     )
   }
 
-  getById(id:number){
-    this.tipoArchivoService.getById(id).subscribe(
-      (data:any) => {
-        this.modal.idTipoArchivo = id
-        this.modal.tipoArchivo = data.tipoArchivo
-        
-      }, (error:any) => {
-        console.log(error);
-      }
-    )
+  // OBTENER TIPO POR ID
+  getById(id: number) {
+    var obj: any = this.tiposArchivos.find((a: TipoArchivo) => a.idTipoArchivo == id)
+    this.modal = { ...obj }
   }
 
-  saveTipoArchivo(){
-
-    if(this.tipoArchivo.tipoArchivo.trim() == '' || this.tipoArchivo.tipoArchivo.trim() == null){
+  saveTipoArchivo() {
+    if (this.tipoArchivo.tipoArchivo.trim() == '' || this.tipoArchivo.tipoArchivo.trim() == null) {
       Swal.fire('Error', 'Digite El nombre Del Tipo De Archivo', 'error')
       return
     }
 
-    this.botonGuardarTipo = true
+    this.tipoArchivoService.save(this.tipoArchivo.tipoArchivo).subscribe(
+      (data: any) => {
+        Swal.fire('Datos Guardados', 'Tipo De Archivo Guardado Con Éxito', 'success')
+        this.tiposArchivos.push(data)
+        console.log(data);
+      }, (error: any) => {
+        Swal.fire('Error', 'Error al Crear El Tipo De Archivo', 'error')
+        console.log(error);
+      }
+    )
 
-    setTimeout(() => {
-      this.tipoArchivoService.save(this.tipoArchivo.tipoArchivo).subscribe(
-        (data:any) => {
-          Swal.fire('Datos Guardados', 'Tipo De Archivo Guardado Con Éxito', 'success')
-          this.botonGuardarTipo = false
-          setTimeout(() => {
-          window.location.reload()
-          }, 2000);
-        }, (error:any) => {
-          Swal.fire('Error', 'Error al Crear El Tipo De Archivo', 'error')
-          this.botonGuardarTipo = false
-          console.log(error);
-        }
-      )
-    }, 2000);
-    
   }
 
-  deleteTipoArchivo(idTipoArchivo:number){
-      Swal.fire({
-        title: 'Eliminar El Tipo De Archivo',
-        text: '¿Estas seguro de El Tipo De Archivo?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Eliminar',
-        cancelButtonText: 'Cancelar'
-      }).then((result) => {
-          if (result.isConfirmed) {
-            this.botonEliminar = true
-            setTimeout(() => {
-              this.tipoArchivoService.delete(idTipoArchivo).subscribe(
-                (data: any) => {
-                  this.tiposArchivos = this.tiposArchivos.filter((tipoArchivo:any) => tipoArchivo.idTipoArchivo != idTipoArchivo);
-                  Swal.fire('Tipo De Archivo Eliminado', 'El Tipo De Archivo ha sido Eliminado Exitosamente', 'success')
-                  this.botonEliminar = false
-                  setTimeout(() => {
-                    window.location.reload()
-                  }, 2000);
-                },
-                (error) => {
-                  Swal.fire('Error', 'Error al Eliminar El Tipo de Archivo', 'error')
-                  this.botonEliminar = false
-                  console.log(error);
-                }
-              )
-            }, 2000);
+  deleteTipoArchivo(idTipoArchivo: number) {
+    Swal.fire({
+      title: 'Eliminar El Tipo De Archivo',
+      text: '¿Estas seguro de El Tipo De Archivo?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.tipoArchivoService.delete(idTipoArchivo).subscribe(
+          (data: any) => {
+            this.tiposArchivos = this.tiposArchivos.filter((tipoArchivo: any) => tipoArchivo.idTipoArchivo != idTipoArchivo);
+            Swal.fire('Tipo De Archivo Eliminado', 'El Tipo De Archivo ha sido Eliminado Exitosamente', 'success')
+          },
+          (error) => {
+            Swal.fire('Error', 'Error al Eliminar El Tipo de Archivo', 'error')
+            console.log(error);
           }
-      })    
+        )
+      }
+    })
   }
 
-  update(){
-    if(this.modal.tipoArchivo.trim() == '' || this.modal.tipoArchivo.trim() == null){
+  update() {
+    if (this.modal.tipoArchivo.trim() == '' || this.modal.tipoArchivo.trim() == null) {
       Swal.fire('Error', 'Digite El nombre Del Tipo De Archivo', 'error')
       return
     }
-    this.botonEditar = true
-    setTimeout(() => {
-      this.tipoArchivoService.update(this.modal).subscribe(
-        (data:any) => {
-          Swal.fire('Datos Guardados', 'El Tipo De Archivo Ha Sido Actualizado', 'success')
-          this.botonEditar = false
-          setTimeout(() => {
-            window.location.reload()
-          }, 2000);
-        }, (error:any) =>{
-          Swal.fire('Error', 'Error al Actualizar el Tipo de Archivo', 'error')
-          this.botonEditar = false
-          console.log(error);
-        }
-      )
-    }, 2000);
+    this.tipoArchivoService.update(this.modal).subscribe(
+      (data: any) => {
+        Swal.fire('Datos Guardados', 'El Tipo De Archivo Ha Sido Actualizado', 'success')
+        var pos = this.tiposArchivos.findIndex((t: TipoArchivo) => t.idTipoArchivo == this.modal.idTipoArchivo)
+        this.tiposArchivos[pos] = data
+        $('#modalEditar').modal('show');
+      }, (error: any) => {
+        Swal.fire('Error', 'Error al Actualizar el Tipo de Archivo', 'error')
+        console.log(error);
+      }
+    )
   }
 
 }
