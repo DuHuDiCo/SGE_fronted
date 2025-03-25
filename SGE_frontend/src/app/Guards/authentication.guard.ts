13138f3329d739c1114fb14fa12ac8acbbd8cf96
Marlon Vivas <mj7marlonvivas@gmail.com>
2025-03-25 16:00:57 -0500

    gfgf

--- SGE_frontend/src/app/Guards/authentication.guard.ts
+++ SGE_frontend/src/app/Guards/authentication.guard.ts
@@ -1,31 +1,43 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../Services/authentication/authentication.service';
 import { OpcionesService } from '../Services/Opciones/opciones.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationGuard implements CanActivate {
-
-  constructor(private authentication: AuthenticationService, private router: Router, private opcionesService:OpcionesService) { }
-
+  constructor(
+    private authentication: AuthenticationService,
+    private router: Router,
+    private opcionesService: OpcionesService
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     if (this.authentication.isLoggedIn()) {
-      var update = this.opcionesService.getUpdate()
-      if(update != null && update == true){
+      var update = this.opcionesService.getUpdate();
+      if (update != null && update == true) {
         return true;
       }
-      
-      this.router.navigate(['cambioContrasena'])
+
+      this.router.navigate(['cambioContrasena']);
       return false;
     }
-    this.router.navigate(['login'])
+    this.router.navigate(['login']);
     return false;
   }
-
 }
